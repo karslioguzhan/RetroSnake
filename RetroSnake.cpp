c7a0a5ffed4ae63fbe6a260227f2f1289e0de29a
Oguzhan <yakusio@googlemail.com>
2023-03-29 14:35:09 +0200

    Add project files.

---
+++ RetroSnake.cpp
@@ -0,0 +1,18 @@
+/*
+    RetroSnake
+    Learning C++ while using Raylib
+    Source: https://www.youtube.com/watch?v=LGqsnM_WEK4
+*/ 
+
+#include <iostream>
+#include <raylib.h>
+
+int main()
+{
+    std::cout << "Staring the Game\n";
+    InitWindow(750, 750, "Retro Snake");
+
+    CloseWindow();
+    return 0;
+}
+
